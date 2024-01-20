c2fb8e76d70d0d385616806e77d90b5b8b7b5948
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-01-20 06:10:47 +0530

    Tailwind check

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ function App() {
 
   return (
     <>
-   
+   <div className="h-14 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+<div className="h-14 bg-gradient-to-r from-sky-500 to-indigo-500"></div>
+<div className="h-14 bg-gradient-to-r from-violet-500 to-fuchsia-500"></div>
+<div className="h-14 bg-gradient-to-r from-purple-500 to-pink-500"></div>
+
+
 
     </>
   )
